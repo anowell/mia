93c5ac71bd3eea17bf78df31a30860d08703c9a7
Anthony Nowell <anowell@gmail.com>
2015-09-28 02:47:56 -0600

    Less build unwrapping: allow build without git tree

--- build.rs
+++ build.rs
@@ -11,20 +11,23 @@ use std::io::Write;
 fn main() {
     // Rustc version
     let rustc = env::var_os("RUSTC").unwrap_or_else(|| OsString::from("rustc"));
-    let rustc_out = Command::new(&rustc)
+    let rustc_version = Command::new(&rustc)
         .arg("--version")
         .output()
-        .unwrap_or_else(|e| { panic!("failed to execute process: {}", e) });
-    let rustc_version = String::from_utf8(rustc_out.stdout).unwrap();
-
+        .into_iter()
+        .flat_map(|out| String::from_utf8(out.stdout))
+        .next()
+        .unwrap_or("rustc unknown-version".into());
 
     // Git SHA
     let git = env::var_os("GIT").unwrap_or_else(|| OsString::from("git"));
-    let git_out = Command::new(&git)
+    let git_sha = Command::new(&git)
         .args(&vec!["rev-parse", "--short", "HEAD"])
         .output()
-        .unwrap_or_else(|e| { panic!("failed to execute process: {}", e) });
-    let git_sha = String::from_utf8(git_out.stdout).unwrap();
+        .into_iter()
+        .flat_map(|out| String::from_utf8(out.stdout))
+        .next()
+        .unwrap_or("no-SHA".into());
 
 
     // Write it all to version.rs
@@ -35,7 +38,7 @@ pub fn get_version() -> &'static str {{
     "algo {pkg_ver} ({git_sha} {build_date})\n{rustc_version}"
 }}
 "#,
-        pkg_ver = option_env!("CARGO_PKG_VERSION").unwrap_or("unknown version"),
+        pkg_ver = option_env!("CARGO_PKG_VERSION").unwrap_or("unknown-version"),
         git_sha = git_sha.trim(),
         build_date = UTC::today().format("%Y-%m-%d"),
         rustc_version = rustc_version.trim(),
