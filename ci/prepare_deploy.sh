f1489200471aedc64c5c551eccccbdd389ff4064
Anthony Nowell <anthony@algorithmia.com>
2017-03-16 09:55:04 -0700

    Future releases should bundle completions
    Future arch installs should include completions now.
    
    Also, the main installer should stop downloading completions
      separately after the next release

--- ci/prepare_deploy.sh
+++ ci/prepare_deploy.sh
@@ -12,6 +12,7 @@ mk_tarball() {
     # Update this part to copy the artifacts that make sense for your project
     # NOTE All Cargo build artifacts will be under the 'target/$TARGET/{debug,release}'
     cp target/$TARGET/release/algo $td
+    cp -r completions $td
 
     pushd $td
 
